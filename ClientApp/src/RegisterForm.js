1b5bafa3b5abd07268f782d742fd8792dbc78976
Jan Dec <82715@student.pb.edu.pl>
2023-11-21 18:18:20 +0100

    Fixing merge

--- ClientApp/src/RegisterForm.js
+++ ClientApp/src/RegisterForm.js
@@ -43,15 +43,15 @@ const RegisterForm = () => {
             email,
             password,
         };
-
-        try {
-            const response = await fetch('https://localhost:7088/api/account/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(userData),
-            });
+        if (valid) {
+            try {
+                const response = await fetch('https://localhost:7088/api/account/register', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(userData),
+                });
 
                 if (response.ok) {
                     console.log('Registration successful!');
