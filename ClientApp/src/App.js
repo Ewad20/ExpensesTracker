85ff0f2ea00c2424a74e86c034f89f163b27a256
Jakub Dąbrowski <jakubdabrowski5701@gmail.com>
2023-11-27 21:16:21 +0100

    PZTRAC-19 TransactionList fix

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterForm from './RegisterForm';
 import RootElement from './RootElement';
-import LoginForm from './LoginForm';
-
+import TransactionList from './TransactionList';
 
 const App = () => {
     return (
@@ -12,8 +11,9 @@ const App = () => {
             <div>
                 <Routes>
                     <Route path="/register" element={<RegisterForm />} />
-                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/transaction" element={<TransactionList />} />
                 </Routes>
+
             </div>
         </Router>
     );
