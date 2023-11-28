7dbadd847e6d3b6cab3fea42843657e83e897f26
Aikoocioq <ola.didluch@o2.pl>
2023-11-28 02:11:54 +0100

    PZTRAC-27

--- ClientApp/src/TransactionList.js
+++ ClientApp/src/TransactionList.js
@@ -2,8 +2,50 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const TransactionList = () => {
-  const [transactions, setTransactions] = useState([]);
-  const walletId = 1; //Id testowe
+    const [transactions, setTransactions] = useState([]);
+    const [startingDate, setStartingDate] = useState(null);
+    const [endingDate, setEndingDate] = useState(null);
+    const [filteredTransactions, setFilteredTransactions] = useState([]);
+    const walletId = 1; //Id testowe
+
+    const handleStartingDateChange = (event) => {
+        setStartingDate(event.target.value);
+    };
+
+    const handleEndingDateChange = (event) => {
+        setEndingDate(event.target.value);
+    };
+
+    const handleFilterClick = () => {
+        if (!startingDate && !endingDate) {
+            setFilteredTransactions(transactions);
+        } else if (!startingDate) {
+            const filtered = transactions.filter((transaction) => {
+                let jsDate = new Date(transaction.Date);
+                return jsDate <= endingDate;
+            });
+            setFilteredTransactions(filtered);
+        } else if (!endingDate) {
+            const filtered = transactions.filter((transaction) => {
+                let jsDate = new Date(transaction.Date);
+                return jsDate >= startingDate;
+            });
+            setFilteredTransactions(filtered);
+        } else {
+            const filtered = transactions.filter((transaction) => {
+                let jsDate = new Date(transaction.Date);
+                if (jsDate >= startingDate && jsDate <= endingDate)
+                    return true;
+                return false;
+            });
+            setFilteredTransactions(filtered);
+        }
+        const sortedTransactions = filteredTransactions.slice().sort((a, b) => {
+            return new Date(b.date) - new Date(a.date);
+        });
+
+        setFilteredTransactions(sortedTransactions);
+    };
 
     useEffect(() => {
         const fetchTransactions = async () => {
@@ -14,6 +56,7 @@ const TransactionList = () => {
                 }
                 const data = await response.json();
                 setTransactions(data);
+                setFilteredTransactions(data);
             } catch (error) {
                 console.error('Error during fetching transactions:', error);
             }
@@ -23,13 +66,32 @@ const TransactionList = () => {
     }, [walletId]);
 
 
+    useEffect(() => {
+        setFilteredTransactions([...filteredTransactions]);
+    }, [filteredTransactions]);
+
   return (
     <div>
-      <h2>Lista transakcji dla portfela {walletId}</h2>
+          <h2>Transaction list for wallet {walletId}</h2>
+          <h3>Filter results:</h3>
+          Starting date:
+          <input
+              type="date"
+              name="startingDatePicker"
+              value={startingDate || ''}
+              onChange={handleStartingDateChange}
+          />
+          <input
+              type="date"
+              name="endingDatePicker"
+              value={endingDate || ''}
+              onChange={handleEndingDateChange}
+          />
+          <button onClick={handleFilterClick}>Filter</button>
       <ul>
-        {transactions.map((transaction) => (
-          <li key={transaction.Id}>
-                ID transakcji: {transaction.Id}, Tytuł: {transaction.Title}, Kwota: {transaction.Amount}
+          {filteredTransactions.map((transaction) => (
+              <li key={transaction.Id}>
+                  Transaction ID: {transaction.Id}, Title: {transaction.Title}, Amount: {transaction.Amount}, Date: {transaction.Date}
           </li>
         ))}
       </ul>
