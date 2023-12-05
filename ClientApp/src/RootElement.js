b9ee4ddfcc1ec6b90f78be2cd3f5c656bb7bd134
edobrzycka <edobrzycka2803@gmail.com>
2023-12-05 10:51:55 +0100

    PZTRAC-77 Dashboard-charts

--- ClientApp/src/RootElement.js
+++ ClientApp/src/RootElement.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import LoginForm from './LoginForm'; 
-import './/styles/Site.css';
-import Calendar from './Calendar';
-import $ from 'jquery'
-import Chart from 'chart.js/auto'
+import LoginForm from './LoginForm';
+import './styles/Site.css';
+import $ from 'jquery';
+import Chart from 'chart.js/auto';
 
 const RootElement = () => {
     const location = useLocation();
@@ -13,14 +12,11 @@ const RootElement = () => {
     const handleLogin = (userData) => {
         setUser(userData);
 
-        //$("#demo-calendar-basic").zabuto_calendar({
-        //    classname: 'table table-bordered lightgrey-weekends'
-        //});
-
         $(document).ready(function () {
             var ctx = document.getElementById("myChart").getContext("2d");
-            const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 150];
-            const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15, 15];
+
+            const xValues = ["Incomes", "Expenses"];
+            const yValues = [4, 8];
 
             new Chart(ctx, {
                 type: "pie",
@@ -29,8 +25,8 @@ const RootElement = () => {
                     datasets: [{
                         fill: false,
                         lineTension: 0,
-                        backgroundColor: "rgba(200,200,200,1.0)",
-                        borderColor: "rgba(0,0,0,0.1)",
+                        backgroundColor: ["pink", "lightblue"],
+                        borderColor: "6C698D",
                         data: yValues
                     }]
                 },
@@ -43,6 +39,37 @@ const RootElement = () => {
             });
         });
     };
+
+    useEffect(() => {
+        if (user) {
+            $(document).ready(function () {
+                var ctx2 = document.getElementById("myChart2").getContext("2d");
+
+                const xValues = ["Food", "Clothes", "Fuel", "Cosmetics"];
+                const yValues = [4, 8, 10, 6];
+
+                new Chart(ctx2, {
+                    type: "bar",
+                    data: {
+                        labels: xValues,
+                        datasets: [{
+                            fill: false,
+                            lineTension: 0,
+                            backgroundColor: ["pink", "lightblue"],
+                            borderColor: "6C698D",
+                           data: yValues,
+                        }],
+                    },
+                    options: {
+                        legend: { display: false },
+                        scales: {
+                            yAxes: [{ ticks: { min: 6, max: 16 } }],
+                        },
+                    },
+                });
+            });
+        }
+    }, [user]);
 
     const handleLogout = () => {
         setUser(null);
@@ -84,7 +111,7 @@ const RootElement = () => {
             <div className="container mt-4">
                 {user && location.pathname === '/' && (
                     <><div className="mb-3">
-                        <span class="fs-4"><strong>Welcome, {user.username}!</strong></span>
+                        <span className="fs-4"><strong>Welcome, {user.username}!</strong></span>
                         <p className="mt-2"><em>Explore your dashboard and manage your financial activities with ease.</em></p>
                         <div className="d-flex mt-3">
                             <Link to="/wallet" className="link me-4">
@@ -120,34 +147,16 @@ const RootElement = () => {
                         </div>
                     </div>
                         <div className="main-page-dashboard">
-                            <div className="dashboard-card dashboard-card__main-section">
-                                <canvas id="myChart" width="400" height="400"></canvas>
+                            <div className="dashboard-card dashboard-card__main-section chart-container2">
+                                <p style={{ fontWeight: 'bold', fontSize: '18px' }}> Incomes & Expences</p>
+                                <canvas id="myChart" width="200" height="200"></canvas>
                             </div>
-                            <div className="dashboard-card dashboard-card__main-section">
-                                <Calendar>
 
-                                </Calendar>
+                            <div className="dashboard-card dashboard-card__main-section chart-container">
+                                <p style={{ fontWeight: 'bold', fontSize: '18px' }}>Expense Categories</p>
+                                <canvas id="myChart2" width="200" height="200"></canvas>
                             </div>
                         </div></>
-                )}
-                {!user && location.pathname === '/' && (
-                    <div className="mt-4">
-                        <LoginForm onLogin={handleLogin} />
-                        <div className="d-flex flex-column align-items-center justify-content-center">
-                            <strong>
-                                <p className="my-text"> If you don't have an account, you can register here:</p>
-                            </strong>
-                            <Link to="/register" className="btn btn-primary">Register</Link>
-                        </div>
-                        <div className="mt-4">
-                            <div className="my-card">
-                                <img className="my-icons" src='https://cdn-icons-png.flaticon.com/128/4256/4256662.png' alt="Icon" />
-                                <p className="my-card-text">
-                                    The Expense Tracker Application is designed to help you effectively manage your finances with ease. Whether you're tracking daily expenses,
-                                    managing multiple wallets, or organizing your financial transactions, this application provides a comprehensive solution for your financial management needs.</p>
-                            </div>
-                        </div>
-                    </div>
                 )}
                 {!user && location.pathname === '/' && (
                     <div className="mt-4">
