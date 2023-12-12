b3f2194f7b9dacefb5332f7326f275b987bb1655
Aikoocioq <ola.didluch@o2.pl>
2023-12-12 14:51:01 +0100

    OlaDEV-Merge

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ETDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("JanConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("OlaConnection")));
 builder.Services.AddCors();
 
 builder.Services.AddAuthentication(options =>
