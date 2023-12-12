cc5c08372eb22061a5264d9c03be849579e6f581
Aikoocioq <ola.didluch@o2.pl>
2023-12-12 16:56:17 +0100

    Connection

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ETDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("JakubConnection")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("OlaConnection")));
 builder.Services.AddCors();
 
 builder.Services.AddAuthentication(options =>
