c543d62cff2acdb28c4fdad00ee01ecd475b3f4c
kasiagrygo <katarzynagrygo@icloud.com>
2023-12-12 19:00:27 +0100

    Naprawiłam chart 2 :)

--- ClientApp/src/Chart2.js
+++ ClientApp/src/Chart2.js
@@ -4,7 +4,7 @@ import Chart from 'chart.js/auto';
 const Chart2 = () => {
 
     const [chart2, setChart2] = useState([]);
-
+    
     useEffect(() => {
         const fetchChart2 = async () => {
             try {
@@ -29,33 +29,40 @@ const Chart2 = () => {
             }
         };
 
-
-        const ctx2 = document.getElementById("myChart2").getContext("2d");
-        const xValues = Object.keys(chart2);
         fetchChart2();
-        console.log(chart2);
-        const yValues = Object.values(chart2);
-
-        new Chart(ctx2, {
-            type: "bar",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: ["pink", "lightblue", "cyan", "grey"],
-                    borderColor: "6C698D",
-                    data: yValues,
-                }],
-            },
-            options: {
-                legend: { display: false },
-                scales: {
-                    yAxes: [{ ticks: { min: 0, max: 16 } }],
-                },
-            },
-        });
     }, []);
+
+    useEffect(() => {
+        console.log(chart2);
+        if (chart2 && Object.keys(chart2).length > 0) {
+            console.log("SIEMA");
+            const ctx2 = document.getElementById("myChart2").getContext("2d");
+            const xValues = Object.keys(chart2);
+            console.log(xValues);
+            const yValues = Object.values(chart2);
+            console.log(yValues);
+
+            const myChart2 = new Chart(ctx2, {
+                type: "bar",
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        fill: false,
+                        lineTension: 0,
+                        backgroundColor: ["pink", "lightblue", "cyan", "grey"],
+                        borderColor: "6C698D",
+                        data: yValues,
+                    }],
+                },
+                options: {
+                    legend: { display: false }
+                },
+            });
+
+            console.log(myChart2);
+        }
+        
+    }, [chart2]);
 
     return (
         <div className="card mx-auto my-5 h-auto background-my w-75"  >
