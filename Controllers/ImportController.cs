450a524bed2aee910952fcf5591f1984bdab7ff0
emek-k <emakorycki@gmail.com>
2023-12-19 10:23:55 +0100

    PZTRAC-54 - Backend importu portfeli

--- Controllers/ImportController.cs
+++ Controllers/ImportController.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using _2023pz_trrepo.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _2023pz_trrepo.Controllers
 {
@@ -14,22 +15,55 @@ namespace _2023pz_trrepo.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpPost("importWallet")]
-        public IActionResult ExportWallet()
+
+        [HttpPost("importWallets")]
+        public async Task<IActionResult> ImportWallets([FromBody] List<ImportedWallet> wallets)
         {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (userId == null)
+            //currentUserId
+            var currentUserId = GetCurrentUserId();
+            //current User
+            var user = await _dbContext.Users
+            .Include("Wallets")
+            .FirstOrDefaultAsync(u => u.Id == currentUserId);
+
+            Console.WriteLine("Received wallets:");
+            //console log information about improted wallets
+            foreach (var wallet in wallets)
             {
-                return StatusCode(401, "Uzytkownik nie jest zalogowany!");
+                if (wallet != null && user != null)
+                {
+                    Wallet newWallet = new()
+                    {
+                        User = user,
+                        UserId = user.Id,
+                        Name = wallet.Name,
+                        IconId = wallet.IconId,
+                        AccountBalance = (double)wallet.AccountBalance,
+                        Incomes = wallet.Incomes,
+                        Expenditures = wallet.Expenditures
+                    };
+
+                    user.Wallets.Add(newWallet);
+                }
+                consoleWriteWalletDetails(wallet);
             }
 
-            return Ok();
+            await _dbContext.SaveChangesAsync();
+            Console.Write("Changes saved!");
+
+            return Ok("Pomyślnie dodano portfele do konta uzytkownika!");
+        }
+
+        private string? GetCurrentUserId()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return userId;
         }
 
         [HttpPost("user")]
         public IActionResult IsUserLogged()
         {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userId = GetCurrentUserId();
             if (userId == null)
             {
                 return StatusCode(401, "Uzytkownik nie jest zalogowany!");
@@ -42,5 +76,39 @@ namespace _2023pz_trrepo.Controllers
         {
             Console.WriteLine("TEST XD");
         }
+
+        public void consoleWriteWalletDetails(ImportedWallet wallet)
+        {
+            Console.WriteLine($"Id: {wallet.Id}, Name: {wallet.Name}, IconId: {wallet.IconId}, AccountBalance: {wallet.AccountBalance}, UserId: {wallet.UserId}");
+            Console.WriteLine("Expenditures:");
+            if (wallet.Expenditures != null)
+            {
+                foreach (var expenditure in wallet.Expenditures)
+                {
+                    Console.WriteLine($"  Id: {expenditure.Id}, Amount: {expenditure.Amount}, Description: {expenditure.Description}");
+                }
+            }
+
+            Console.WriteLine("Incomes:");
+            if (wallet.Incomes != null)
+            {
+                foreach (var income in wallet.Incomes)
+                {
+                    Console.WriteLine($"  Id: {income.Id}, Amount: {income.Amount}, Description: {income.Description}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+    public class ImportedWallet
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int IconId { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string? UserId { get; set; }
+        public List<Expenditure>? Expenditures { get; set; }
+        public List<Income>? Incomes { get; set; }
     }
 }
