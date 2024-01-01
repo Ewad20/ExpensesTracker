ed608ccd567a9a3486b64bff206e77c2a6c9dcf8
edobrzycka <edobrzycka2803@gmail.com>
2024-01-01 13:46:33 +0100

    Sprint6

--- ClientApp/src/TransactionForm.js
+++ ClientApp/src/TransactionForm.js
@@ -61,7 +61,6 @@ const TransactionForm = ({ onSubmit, onCancel, walletId }) => {
     const handleTypeSelection = (type) => {
         setTransactionType(type);
         setFormVisible(true);
-        // Filter categories based on the selected type
         const filteredCategories = categories.filter(category => {
             return type === 'income' ? category.Type === 'Income' : category.Type === 'Expenditure';
         });
